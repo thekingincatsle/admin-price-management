214a2405241f58b0e027461bffc962794bde9be5
Dung Vu <dungv.vu@mission.plus>
2023-01-11 14:42:49 +0700

    fix: issue

--- src/containers/Landing.jsx
+++ src/containers/Landing.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Landing = () => {
   const admin = JSON.parse(localStorage.getItem("admin"));
   return (
-    <div className="w-full h-full bg-cover flex justify-center text-3xl mt-10">
+    <div className="w-full h-full bg-cover flex flex-col items-center text-3xl mt-10 gap-5">
       <div>Xin chào admin {admin.ten}</div>
       <div>Email {admin.email}</div>
     </div>
