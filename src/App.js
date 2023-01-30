11b66fce4470469cb5f9b7ac3b0ba32326905ad8
thekingincatsle <dat.dt200133@sis.hust.edu.vn>
2023-01-31 00:06:13 +0700

    add env

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ const App = () => {
       <Routes>
         <Route path="/*" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </div>
   );
